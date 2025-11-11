bdd3a89eedf5126aa6eedba44f2e30e899443378
DavgaaDavka08 <iderbatdagvadorj@gmail.com>
2025-11-11 09:07:05 +0800

    js-ended

---
+++ index.js
@@ -0,0 +1,45 @@
+///Variable
+// let names = "dagvadorj"; //let -huvisagch name- huvisagchiin ner dagvadorj- value==
+// let year = 2009;
+// let now = 2025;
+// let age = now - year;
+// console.log(age);
+
+// let daySalary = 100000;
+
+// let workDay = 20;
+
+// let first = 10;
+// let second = 5;
+// let printl = first - second;
+// console.log("object :>> ", printl);
+let a = 10;
+let b = 20;
+let c = 30;
+let sum = a + b + c;
+console.log("sum :>> ", sum);
+
+//if else \
+let age = 18;
+
+if (age >= 18) {
+  console.log("ta nasand hurseen");
+} else {
+  console.log("ta nsand hureegui ");
+}
+//2тэгш эсвэл сондгойг шалгадаг　if else бич.
+//2 хүйтэн эсвэл дулаан шалгадаг if else 0 - t
+
+let num = 8;
+if (num % 2 == 0) {
+  console.log("tegsh");
+} else {
+  console.log("sondgoi ");
+}
+
+let temp = 5;
+if (temp > 0) {
+  console.log("dulaahan");
+} else {
+  console.log("huiten");
+}
